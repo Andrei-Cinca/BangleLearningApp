92e46eaa5173a15f082c59a5f8b3c124e3baff3d
Andrei Cinca <andrei-marian.cinca@kcl.ac.uk>
2023-02-15 22:19:43 +0000

    add interface to app

--- apps/LearnHelper/app.js
+++ apps/LearnHelper/app.js
@@ -1,16 +1,34 @@
-E.showMessage("My\nSimple\nApp","LearnHelper")
-
-var lastBtn;
-setWatch(function() {
-  if (lastBtn && lastBtn.time > getTime() - 1) {
-    // Ignore duplicate presses within 1 second
-    return;
-  }
-  lastBtn = {
-    time: getTime(),
-    btn: "side"
-  };
-  // Code to go back to home screen
-  g.reset(); // Clear the screen
-  Bangle.drawWidgets(); // Redraw the widgets
-}, BTN1, {repeat:true});
+// Two variables to update
+var boolean = false;
+var number = 50;
+// First menu
+var mainmenu = {
+  "" : {
+    "title" : "-- Main Menu --"
+  },
+  "Beep" : function() { Bangle.beep(); },
+  "Buzz" : function() { Bangle.buzz(); },
+  "Submenu" : function() { E.showMenu(submenu); },
+  "A Boolean" : {
+    value : boolean,
+    format : v => v?"On":"Off",
+    onchange : v => { boolean=v; }
+  },
+  "A Number" : {
+    value : number,
+    min:0,max:100,step:10,
+    onchange : v => { number=v; }
+  },
+  "Exit" : function() { E.showMenu(); },
+};
+// Submenu
+var submenu = {
+  "" : {
+    "title" : "-- SubMenu --"
+  },
+  "One" : undefined, // do nothing
+  "Two" : undefined, // do nothing
+  "< Back" : function() { E.showMenu(mainmenu); },
+};
+// Actually display the menu
+E.showMenu(mainmenu);
