6c3e2c843f7676cc48837abfc6ced8c979b3118c
Andrei Cinca <andrei-marian.cinca@kcl.ac.uk>
2023-02-15 22:06:54 +0000

    add home shortcut

--- apps/LearnHelper/app.js
+++ apps/LearnHelper/app.js
@@ -1,1 +1,16 @@
 E.showMessage("My\nSimple\nApp","LearnHelper")
+
+var lastBtn;
+setWatch(function() {
+  if (lastBtn && lastBtn.time > getTime() - 1) {
+    // Ignore duplicate presses within 1 second
+    return;
+  }
+  lastBtn = {
+    time: getTime(),
+    btn: "side"
+  };
+  // Code to go back to home screen
+  g.reset(); // Clear the screen
+  Bangle.drawWidgets(); // Redraw the widgets
+}, BTN1, {repeat:true});
